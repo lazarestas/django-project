6f356724800690b32826cafdcfb82ef5a55cf587
lazarestas <80478760+lazarestas@users.noreply.github.com>
2022-11-28 08:38:40 +0300

    делаю библиотеку по методичке, сначала настраиваем редирект на catalog

--- djangofirst/urls.py
+++ djangofirst/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
 ]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+
