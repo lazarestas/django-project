87990a2a3c4bea43fcc5561aadf4415824f0c3d3
lazarestas <80478760+lazarestas@users.noreply.github.com>
2022-11-29 13:18:29 +0300

    настроил примерную табличку books и добавил туда запись

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Books(models.Model):
+    """A typical class defining a model, derived from the Model class."""
+    # Fields
+    uid = models.AutoField(primary_key=True, default=True)
+    title = models.CharField(max_length=20, help_text='Enter book title')
+    pubdate = models.DateTimeField(auto_now_add=True)
+    book_file = models.FilePathField(help_text='Path to book file (needs testing)')
+    # …
+    # Metadata
+
+    class Meta:
+        ordering = ['title', '-pubdate']
+
+    # Methods
+    def get_absolute_url(self):
+        """Returns the URL to access a particular instance of MyModelName."""
+        return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the MyModelName object (in Admin site etc.)."""
+        return self.title
